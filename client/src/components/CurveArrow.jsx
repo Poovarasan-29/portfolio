68bf1eaf59f6632ae48df4c86b8a9a2def46f67c
Poovarasan-29 <spking222005@gmail.com>
2025-01-26 22:40:20 +0530

    v2.0.1 - Project section curve arrow added

--- client/src/components/CurveArrow.jsx
+++ client/src/components/CurveArrow.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CurveArrow = () => {
   return (
     <div className="curve-arrow">
-      {/* <svg width="400" height="300" xmlns="http://www.w3.org/2000/svg">
+      <svg width="400" height="300" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M0 10 C 50 150, 100 150, 100 100 
         S 200 50, 200 100 
@@ -14,7 +14,7 @@ const CurveArrow = () => {
           strokeDasharray="10,10" // Makes the line dotted
         />
         <polygon points="350,200 342,194 342,216" fill="orange" />
-      </svg> */}
+      </svg>
     </div>
   );
 };
