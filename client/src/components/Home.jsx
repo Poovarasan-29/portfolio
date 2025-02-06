0c217fc89f346443bf3ffee1169d8c0b5a495675
Poovarasan-29 <spking222005@gmail.com>
2025-02-07 00:49:25 +0530

    v2.2 - Resume pdf and Resume button design changed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,18 @@
+import { Button } from "@mui/material";
+import { useState } from "react";
+
 export default function Home({ classname }) {
+  const [changeIconHover, setChangeIconHover] = useState(
+    "ps-2 pt-1 bi bi-cloud-arrow-down"
+  );
   return (
     <div className={`home ms-4 ${classname}`}>
       <h1 className="gradient-text display-3">Hello</h1>
       <h1 className="gradient-text display-3">I'm Poovarasan</h1>
-      <h3 className="gradient-text mern-stack-text display-6">MERN Stack Developer</h3>
-      <a
-        href="/Resume.pdf"
-        className="btn btn-primary"
-        download="PoovarasanCV.pdf"
-      >
+      <h3 className="gradient-text mern-stack-text display-6">
+        MERN Stack Developer
+      </h3>
+      <a href="/PoovarasanS.pdf" className="btn" download="PoovarasanCV.pdf">
         <div className="animate-btn">
           <span className="bubble"></span>
           <span className="bubble"></span>
@@ -16,7 +20,31 @@ export default function Home({ classname }) {
           <span className="bubble"></span>
           <span className="bubble"></span>
         </div>
-        <span className="download-text">Download CV</span>
+        {/* <span className="download-text">
+          Resume <i className="bi bi-cloud-arrow-down"></i>
+        </span> */}
+        <Button
+          variant="contained"
+          color="primary"
+          style={{
+            textTransform: "capitalize",
+            padding: "4px 14px",
+            fontSize: "18px",
+          }}
+          onMouseEnter={() =>
+            setChangeIconHover("ps-2 pt-1 bi bi-cloud-arrow-down-fill")
+          }
+          onMouseLeave={() =>
+            setChangeIconHover("ps-2 pt-1 bi bi-cloud-arrow-down")
+          }
+        >
+          Resume{" "}
+          <i
+            className={changeIconHover}
+            style={{ transform: "scale(1.4)" }}
+          ></i>
+        </Button>
+        {/* <span className="download-text">Download CV</span> */}
       </a>
     </div>
   );
